bb662b36df1354a20295c2c6a59260c44c231c13
unknown <mututadaniel54@gmail.com>
2024-04-23 01:07:56 -0700

    changes

---
+++ app.js
@@ -0,0 +1,20 @@
+
+// const mainmenu = document.querySelector('.mainmenu');
+// const closemenu = document.querySelector('.closemenu');
+// const openmenu = document.querySelector('.openmenu');
+
+
+
+// openmenu.addEventListener('click',show);
+// closemenu.addEventListener('click',close);
+
+// function show(){
+//     mainmenu.Style.display = 'flex';
+//     mainmenu.Style.top = '0';
+    
+// }
+
+
+// function close() {
+//     mainmenu.Style.top = '-100%';
+// }
